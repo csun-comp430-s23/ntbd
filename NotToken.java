f53eeb682d5eb347391cbf3bfaca4119b2ead114
janelletio98 <43478743+janelletio98@users.noreply.github.com>
2023-03-31 22:17:47 -0700

    Not Token & Semi Colon Token Files Added

---
+++ NotToken.java
@@ -0,0 +1,16 @@
+public class NotToken implements Token{
+    @Override
+    public boolean equals(final Object other) {
+        return other instanceof NotToken;
+    }
+
+    @Override
+    public int hashCode() {
+        return 24;
+    }
+
+    @Override
+    public String toString() {
+        return "NotToken";
+    }
+}
